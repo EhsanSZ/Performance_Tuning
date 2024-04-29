f4cbcbdec2249200b1837c1f1d31cfb7516f1e0e
EhsanSZ <e.seyedzadeh73@gmail.com>
2024-04-29 20:21:09 +0330

    start

---
+++ 1.Start/03-BufferPool.sql
@@ -0,0 +1,61 @@
+﻿
+--SQL Server مشاهده قسمت های مختلف حافظه در 
+DBCC MemoryStatus
+GO
+---------------------
+USE master
+GO
+--بررسي جهت وجود بانك اطلاعاتي و حذف آن
+USE master
+GO
+IF DB_ID('MyDB2017')>0
+BEGIN
+	ALTER DATABASE MyDB2017 SET SINGLE_USER WITH ROLLBACK IMMEDIATE
+	DROP DATABASE MyDB2017
+END
+GO
+CREATE DATABASE MyDB2017
+GO
+USE MyDB2017
+GO
+---------------------
+--Create Table
+--بررسي جهت وجود جدول و بررسي آن
+IF OBJECT_ID('Test_Table')>0
+	DROP TABLE Test_Table
+GO	
+--ايجاد جدول
+CREATE TABLE Test_Table 
+(
+   FirstName CHAR(1000),
+   LastName  CHAR(1000),
+   Email     CHAR(1000),   
+)
+GO
+--درج تعدادي داده تستي در جدول
+INSERT INTO Test_Table(FirstName,LastName,Email) VALUES 
+	('Ehsan','Seyedzadeh','TestMail@gmail.com')
+GO 1000
+--مشاهده ركوردهاي جدول
+SELECT * FROM Test_Table
+GO
+---------------------
+--به ازای هر بانک اطلاعاتی Buffer Pool مشاهده فضای تخصیص داده شده در 
+;WITH src AS
+(
+	SELECT
+		database_id, db_buffer_pages = COUNT_BIG(*)
+		FROM sys.dm_os_buffer_descriptors
+		GROUP BY database_id
+)
+SELECT
+	[db_name] = CASE [database_id] WHEN 32767
+		THEN 'Resource DB'
+		ELSE DB_NAME([database_id]) END,
+	db_buffer_pages,
+	db_buffer_MB = CAST(db_buffer_pages / 128.0 AS DECIMAL(6,2))
+FROM src
+ORDER BY db_buffer_MB DESC;
+GO
+
+
---
+++ 1.Start/04-Checkpoint.sql
@@ -0,0 +1,45 @@
+﻿
+--Checkpoint بررسی انواع 
+GO
+--Manual Checkpoint (دستی Checkpoint)
+--به ازای یک بانک اطلاعاتی خاص رخ می دهد
+USE MyDB2017
+GO
+CHECKPOINT
+GO
+CHECKPOINT 5
+GO
+---------------------
+--Automatic Checkpoint (اتوماتیک Checkpoint)
+SP_CONFIGURE 'recovery interval (min)'
+GO
+SP_CONFIGURE 'recovery interval (min)', 15
+GO
+RECONFIGURE
+---------------------
+--Indirect Checkpoint (غیر مستقیم Checkpoint)
+/*
+استفاده نمی شود Automatic Checkpoint در این حالت از 
+به بعد SQL Server 2012 ارائه از 
+SSMS بررسی در 
+
+ALTER DATABASE database_name SET 
+	TARGET_RECOVERY_TIME = target_recovery_time { SECONDS | MINUTES}
+*/
+ALTER DATABASE MyDB2017 SET 
+	TARGET_RECOVERY_TIME = 50 SECONDS 
+GO
+---------------------
+--Internal Checkpoint (داخلی Checkpoint)
+/*
+غیر قابل کنترل توسط کاربر می باشدCheckpoint این نوع
+می تواند رخ دهدCheckpoint در حالت های زیر این نوع 
+
+Some database files have been modified (removed or added by T-SQL command ALTER DATABASE)
+Database backup is in progress
+Database snapshot is being created
+Shutdown operation occurred on all databases except when Shutdown is not clean (with NOWAIT)
+Recovery model has been changed from Full or Bulk-Logged to Simple
+Database log is 70% full (applies only to Simple recovery model)
+Minimally logged operation executed (applies only to Bulk-Logged recovery model)
+*/
