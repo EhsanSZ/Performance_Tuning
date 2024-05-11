9fb022a2b5eb2ecb086cc1fc4a1e02e0b53a4103
EhsanSZ <e.seyedzadeh73@gmail.com>
2024-05-12 01:01:40 +0330

    Columnstore Index

---
+++ 14 ColumnstoreIndex/01-PartitionedView.sql
@@ -0,0 +1,131 @@
+﻿
+USE master
+GO
+IF DB_ID('DB1')>0
+BEGIN
+	ALTER DATABASE DB1 SET SINGLE_USER WITH ROLLBACK IMMEDIATE
+	DROP DATABASE DB1
+END
+GO
+IF DB_ID('DB2')>0
+BEGIN
+	ALTER DATABASE DB2 SET SINGLE_USER WITH ROLLBACK IMMEDIATE
+	DROP DATABASE DB2
+END
+GO
+IF DB_ID('DB3')>0
+BEGIN
+	ALTER DATABASE DB3 SET SINGLE_USER WITH ROLLBACK IMMEDIATE
+	DROP DATABASE DB3
+END
+GO
+CREATE DATABASE DB1
+CREATE DATABASE DB2
+CREATE DATABASE DB3
+GO
+--------------------------------------------------------------------
+USE master
+GO
+--ساخت جدول پرسنل برای سرور اول با شرط ورود كارمندانی كه دارای كد پرسنلی خاص هستند
+CREATE TABLE DB1.dbo.Employees 
+(
+	EmployeeID INT PRIMARY KEY CHECK (EmployeeID BETWEEN 1 AND 100),
+	FirtName NVARCHAR(100),
+	LastName NVARCHAR(100)
+)
+GO
+--ساخت جدول پرسنل برای سرور دوم با شرط ورود كارمندانی كه دارای كد پرسنلی خاص هستند
+CREATE TABLE DB2.dbo.Employees 
+(
+	EmployeeID INT PRIMARY KEY CHECK (EmployeeID BETWEEN 101 AND 200),
+	FirtName NVARCHAR(100),
+	LastName NVARCHAR(100)
+)
+GO
+--ساخت جدول پرسنل برای سرور سوم با شرط ورود كارمندانی كه دارای كد پرسنلی خاص هستند
+CREATE TABLE DB3.dbo.Employees 
+(
+	EmployeeID INT PRIMARY KEY CHECK (EmployeeID BETWEEN 201 AND 300),
+	FirtName NVARCHAR(100),
+	LastName NVARCHAR(100)
+)
+GO
+--------------------------------------------------------------------
+--DB1 درج دیتا در 
+INSERT INTO DB1.dbo.Employees VALUES (1,N'مسعود',N'طاهری')
+INSERT INTO DB1.dbo.Employees VALUES (2,N'فرید',N'طاهری')
+INSERT INTO DB1.dbo.Employees VALUES (3,N'مجید',N'طاهری')
+INSERT INTO DB1.dbo.Employees VALUES (4,N'علی',N'طاهری')
+INSERT INTO DB1.dbo.Employees VALUES (120,N'علیرضا',N'طاهری')--شرط را چك كنید
+GO
+--DB2 درج دیتا در 
+INSERT INTO DB2.dbo.Employees VALUES (120,N'علیرضا',N'طاهری')
+INSERT INTO DB2.dbo.Employees VALUES (121,N'سامان',N'حسینی')
+INSERT INTO DB2.dbo.Employees VALUES (122,N'محمد',N'نوری')
+INSERT INTO DB2.dbo.Employees VALUES (123,N'بهرام',N'غفاری')
+INSERT INTO DB2.dbo.Employees VALUES (250,N'علی',N'بسطامی')--شرط را چك كنید
+GO
+--DB3 درج دیتا در 
+INSERT INTO DB3.dbo.Employees VALUES (250,N'علی',N'بسطامی')
+INSERT INTO DB3.dbo.Employees VALUES (251,N'نادر',N'بیرونی')
+INSERT INTO DB3.dbo.Employees VALUES (252,N'كریم',N'مقدادی')
+INSERT INTO DB3.dbo.Employees VALUES (253,N'محمد',N'عطایی')
+GO
+--بررسی داده های موجود در جداول
+SELECT EmployeeID,FirtName,LastName FROM DB1.dbo.Employees
+SELECT EmployeeID,FirtName,LastName FROM DB2.dbo.Employees
+SELECT EmployeeID,FirtName,LastName FROM DB3.dbo.Employees
+GO
+/*
+Union ساخت یک کوئری با 
+Execution Plan نمایش 
+*/
+SELECT EmployeeID,FirtName,LastName FROM DB1.dbo.Employees
+UNION
+SELECT EmployeeID,FirtName,LastName FROM DB2.dbo.Employees
+UNION
+SELECT EmployeeID,FirtName,LastName FROM DB3.dbo.Employees
+GO
+--------------------------------------------------------------------
+--DB1 ایجاد یک ویو پارتیشن شده در 
+USE DB1
+GO
+--Union استفاده از 
+CREATE OR ALTER VIEW View_AllEmployees1
+AS
+	SELECT EmployeeID,FirtName,LastName FROM DB1.dbo.Employees
+	UNION
+	SELECT EmployeeID,FirtName,LastName FROM DB2.dbo.Employees
+	UNION
+	SELECT EmployeeID,FirtName,LastName FROM DB3.dbo.Employees
+GO
+--Union All استفاده از 
+CREATE OR ALTER VIEW View_AllEmployees2
+AS
+	SELECT EmployeeID,FirtName,LastName FROM DB1.dbo.Employees
+	UNION ALL
+	SELECT EmployeeID,FirtName,LastName FROM DB2.dbo.Employees
+	UNION ALL
+	SELECT EmployeeID,FirtName,LastName FROM DB3.dbo.Employees
+GO
+/*
+اجرای کوئری 
+Execution Plan بررسی 
+*/
+SELECT * FROM View_AllEmployees1
+SELECT * FROM View_AllEmployees2 
+GO
+SELECT * FROM View_AllEmployees1 WHERE EmployeeID BETWEEN 1 AND 50
+SELECT * FROM View_AllEmployees2 WHERE EmployeeID BETWEEN 1 AND 50
+GO
+SELECT * FROM View_AllEmployees1 WHERE EmployeeID BETWEEN 1 AND 200
+SELECT * FROM View_AllEmployees2 WHERE EmployeeID BETWEEN 1 AND 200
+GO
+GO
+--پاک کردن همه دیتابیس ها
+USE master
+GO
+DROP DATABASE IF EXISTS DB1
+DROP DATABASE IF EXISTS DB2
+DROP DATABASE IF EXISTS DB3
+GO
---
+++ 14 ColumnstoreIndex/02-LinkedServer.sql
@@ -0,0 +1,213 @@
+﻿
+/*
+Linked Server استفاده از 
+دیگر Instance برای اتصال به یک Linked Server ایجاد 
+*/
+USE master
+GO
+EXEC sp_addlinkedserver     
+   @server=N'SQL2016',   
+   @srvproduct=N'',  
+   @provider=N'SQLNCLI',   
+   @datasrc=N'127.0.0.1\sqlserver2016';
+GO
+EXEC master.dbo.sp_addlinkedsrvlogin @rmtsrvname = N'SQL2016', 
+	@locallogin = NULL , @useself = N'False', 
+	@rmtuser = N'sa', @rmtpassword = N'123456'
+GO
+--ها و لاگین های مربوط به آنLinked Server بدست آوردن لیست 
+SELECT * FROM SYS.servers
+SELECT * FROM SYS.linked_logins
+GO
+/*
+--Linked Server شکل کلی دسترسی به اشیاء در  
+LinkedServer.Database.Schema.Objectname
+*/
+SELECT * FROM LinkedServerName.DatabaseName.dbo.TableName
+GO
+--------------------------------------------------------------------
+/*
+Linked Server ساخت جدول در دیتابیس 
+SQL Server 2016
+*/
+GO
+USE AdventureworksDW2016CTP3
+GO
+DROP TABLE IF EXISTS TestStudent
+GO
+CREATE TABLE TestStudent
+(
+	StudentID INT PRIMARY KEY,
+	FullName NVARCHAR(100)
+)
+GO
+INSERT INTO TestStudent(StudentID,FullName) VALUES
+	(1,N'مسعود طاهری')
+GO
+SELECT * FROM TestStudent
+GO
+--------------------------------------------------------------------
+/*
+Linked Server با استفاده از CRUD انجام عملیات 
+Show Execution Plan
+SQLProfiler (SQLServer2016 ** Filter LoginName=sa)
+*/
+USE AdventureWorksDW2017
+SET STATISTICS IO ON
+SET STATISTICS TIME ON
+GO
+/*
+ساده Select اجرای یک 
+Select Query
+Show Execution Query
+Show SQL Server Profiler
+*/
+SELECT 
+	* 
+FROM SQL2016.AdventureworksDW2016CTP3.dbo.FactInternetSales
+GO
+/*
+ساده با شرط Select اجرای یک 
+Select Query
+Show Execution Query
+Show SQL Server Profiler
+*/
+SELECT 
+	* 
+FROM SQL2016.AdventureworksDW2016CTP3.dbo.FactInternetSales
+WHERE SalesOrderNumber='SO68464'
+GO
+/*
+ساده با شرط Select اجرای یک 
+Select Query
+Show Execution Query
+Show SQL Server Profiler
+بحث تخمین اشتباه
+*/
+SELECT 
+	* 
+FROM SQL2016.AdventureworksDW2016CTP3.dbo.FactInternetSales FactInternetSales
+INNER JOIN AdventureWorksDW2017..DimCustomer ON
+	FactInternetSales.CustomerKey=DimCustomer.CustomerKey
+WHERE FactInternetSales.SalesOrderNumber='SO68464'
+GO
+/*
+ساده با شرط Select اجرای یک 
+Select Query
+Show Execution Query
+Show SQL Server Profiler
+بحث تخمین اشتباه
+Linked Server استفاده هر دو جدول از 
+*/
+SELECT 
+	* 
+FROM SQL2016.AdventureworksDW2016CTP3.dbo.FactInternetSales FactInternetSales
+INNER JOIN SQL2016.AdventureworksDW2016CTP3.dbo.DimCustomer DimCustomer ON
+	FactInternetSales.CustomerKey=DimCustomer.CustomerKey
+WHERE FactInternetSales.SalesOrderNumber='SO68464'
+GO
+-------------------------------------
+/*
+اجرای دستور برای درج رکورد
+Insert Query
+Show Execution Query
+Show SQL Server Profiler
+*/
+INSERT INTO SQL2016.AdventureworksDW2016CTP3.dbo.TestStudent(StudentID,FullName) VALUES
+	(2,N'فرید طاهری')
+GO
+/*
+اجرای دستور برای درج رکورد
+Insert Query
+Show Execution Query
+Show SQL Server Profiler
+*/
+INSERT INTO SQL2016.AdventureworksDW2016CTP3.dbo.TestStudent(StudentID,FullName) 
+	SELECT 
+		object_id,NAME 
+	FROM SYS.objects
+GO
+-------------------------------------
+/*
+اجرای دستور برای به روز رسانی رکورد
+Update Query
+Show Execution Query
+Show SQL Server Profiler
+*/
+UPDATE SQL2016.AdventureworksDW2016CTP3.dbo.TestStudent SET
+	FullName=N'فرید طاهری*'
+WHERE StudentID=2
+GO
+/*
+اجرای دستور برای به روز رسانی رکورد
+Update Query
+Show Execution Query
+Show SQL Server Profiler
+*/
+UPDATE TestStudent
+ SET 
+	TestStudent.FullName=OBJ.NAME
+FROM SQL2016.AdventureworksDW2016CTP3.dbo.TestStudent TestStudent
+INNER JOIN SYS.objects OBJ ON
+	TestStudent.StudentID=OBJ.object_id
+WHERE TestStudent.StudentID=2
+GO
+--------------------------------------------------------------------
+--------------------------------------------------------------------
+--------------------------------------------------------------------
+--OpenQuery استفاده از تابع 
+/*
+استفاده از آن به جنس کاری که انجام می دهیم بستگی دارد
+*/
+GO
+SELECT 
+	FactInternetSales.*
+FROM OPENQUERY
+(
+	SQL2016,
+	'SELECT * FROM AdventureworksDW2016CTP3.dbo.FactInternetSales' 
+) AS FactInternetSales
+GO
+--Linked Server استفاده مستقیم از 
+SELECT 
+	* 
+FROM SQL2016.AdventureworksDW2016CTP3.dbo.FactInternetSales
+GO
+-------------------------------------
+--OpenQuery استفاده از تابع 
+SELECT 
+	FactInternetSales.*
+FROM OPENQUERY
+(
+	SQL2016,
+	'SELECT * FROM AdventureworksDW2016CTP3.dbo.FactInternetSales
+		WHERE SalesOrderNumber=''SO68464'''
+) AS FactInternetSales
+GO
+--Linked Server استفاده مستقیم از 
+SELECT 
+	* 
+FROM SQL2016.AdventureworksDW2016CTP3.dbo.FactInternetSales
+WHERE SalesOrderNumber='SO68464'
+GO
+-------------------------------------
+--OpenQuery استفاده از تابع 
+SELECT 
+	FactInternetSales.*
+FROM OPENQUERY
+(
+	SQL2016,
+	'SELECT * FROM AdventureworksDW2016CTP3.dbo.FactInternetSales
+		WHERE SalesOrderNumber=''SO68464'''
+) AS FactInternetSales
+INNER JOIN AdventureWorksDW2017..DimCustomer ON
+	FactInternetSales.CustomerKey=DimCustomer.CustomerKey
+GO
+--Linked Server استفاده مستقیم از 
+SELECT 
+	* 
+FROM SQL2016.AdventureworksDW2016CTP3.dbo.FactInternetSales FactInternetSales
+INNER JOIN AdventureWorksDW2017..DimCustomer ON
+	FactInternetSales.CustomerKey=DimCustomer.CustomerKey
+WHERE FactInternetSales.SalesOrderNumber='SO68464'
+GO
